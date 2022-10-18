8d621b21415ae2db96be8c351a6e7596cea531cb
abdec2 <abdec2@hotmail.com>
2022-10-18 09:31:46 +0300

    done

---
+++ src/abi/config.js
@@ -0,0 +1,12 @@
+
+const CONFIG = {
+    chainId: 5,
+    // contractAddress: "0xa6CbC98Bb8AaAb6A54Ff2EF915152e4ba6d8D551", 
+    contractAddress: "0xe057EA9Fa49f990a39152778B7Cf4BC0D9231Cb2",
+    tokenAddress: "0x40fd7FA8b89a7B5B825988D189bA87aDa1B13E39", 
+    tokenDecimals: '18',
+    tokenSymbol: 'DGRN', 
+    networkName: 'Goerli Testnet'
+}
+
+export default CONFIG;
